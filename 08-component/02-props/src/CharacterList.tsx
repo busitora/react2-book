6bb9048b7935ad7b2167a44b1491b258852ef4c2
busitora <ts12272530@gmail.com>
2021-01-24 18:30:53 +0900

    arrow演算子

--- 08-component/02-props/src/CharacterList.tsx
+++ 08-component/02-props/src/CharacterList.tsx
@@ -3,9 +3,10 @@ import { Header, Icon, Item } from 'semantic-ui-react';
 
 export type Character = {
   id: number;
-  name: string;
+  name?: string;
   grade: number;
   height?: number;
+  // 身長要素は省略できる
 };
 
 type Props = {
@@ -13,29 +14,27 @@ type Props = {
   characters: Character[];
 };
 
-const CharacterList: FC<Props> = (props) => {
-  const { school, characters } = props;
-
-  return (
-    <>
-      <Header as="h2">{school}</Header>
-      <Item.Group>
-        {characters.map((character) => (
-          <Item key={character.id}>
-            <Icon name="user circle" size="huge" />
-            <Item.Content>
-              <Item.Header>{character.name}</Item.Header>
-              <Item.Meta>{character.grade}年生</Item.Meta>
-              <Item.Meta>
-                {character.height ? character.height : '???'}
-                cm
-              </Item.Meta>
-            </Item.Content>
-          </Item>
-        ))}
-      </Item.Group>
-    </>
-  );
-};
+const CharacterList: FC<Props> = ({ school, characters }) => (
+  <>
+    {/* ↑はフラグメント、不要なdivの生成を防ぐ */}
+    <Header as="h2">{school}</Header>
+    {/* <Header>, <Item>, <Icon> は Semantic UI React のコンポーネント */}
+    <Item.Group>
+      {characters.map((character) => (
+        <Item key={character.id}>
+          <Icon name="user circle" size="huge" />
+          <Item.Content>
+            <Item.Header>{character.name ?? '不明'}</Item.Header>
+            <Item.Meta>{character.grade}年生</Item.Meta>
+            <Item.Meta>
+              {character.height ?? '???'}
+              cm
+            </Item.Meta>
+          </Item.Content>
+        </Item>
+      ))}
+    </Item.Group>
+  </>
+);
 
 export default CharacterList;
